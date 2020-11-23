e12d0ee1a8f10235e30f9382d12fe30be23e023c
markid1ggwp <markmopairat12345@gmail.com>
2020-11-23 23:02:31 +0700

    rhrhrh

---
+++ src/player/Job.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package player;
+
+/**
+ * @version 2
+ *เป็นตัวที่เก็บรูปเเบบของอาชีพ
+ * @author benbe
+ */
+public enum Job {
+    Swordman,Gunner,Magician,Boxing
+}
