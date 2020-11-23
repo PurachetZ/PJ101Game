1c765f92e3b8ff481b9cc484e625a9bfc4249545
markid1ggwp <markmopairat12345@gmail.com>
2020-11-23 23:02:24 +0700

    ghfhfh

---
+++ src/player/Hit.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package player;
+
+/**
+ * @version 1
+ *เป็นตัวที่เก็บสถานะของcri
+ * @author mark
+ */
+public enum Hit{
+    Super,normal,miss;
+}
